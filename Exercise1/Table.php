e881b1679c27a6abbc414f7eb59a043b00d82b24
Nick North <jnnorth1998@ku.edu>
2019-10-07 08:59:39 -0500

    Created multiplication table using php

---
+++ Exercise1/Table.php
@@ -0,0 +1,37 @@
+>?php
+
+  echo "<table>";
+
+  for ($row = 0; $row < 101; $row++)
+  {
+    echo "<tr>";
+
+    for ($col = 0; $col < 101; $col++)
+    {
+      if ($row == 0 && $col == 0)
+      {
+        echo "<th></th>";
+      }
+
+      elseif ($row != 0 && $col == 1)
+      {
+        echo "<th> $row </th>";
+      }
+      elseif ($row == 1 && $col != 0)
+      {
+        echo "<th> $col </th>";
+      }
+
+      else
+      {
+        $product = $row * $col;
+        echo "<td> $product </td>";
+      }
+    }
+
+    echo "/tr";
+  }
+
+  echo "/table";
+
+?>
